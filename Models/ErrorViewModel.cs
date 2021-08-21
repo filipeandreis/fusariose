cfa21b73f6d4a95d0db2c130b1913823f0f060c1
unknown <filipe.andreis@live.com>
2021-08-21 11:40:40 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projeto_netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
