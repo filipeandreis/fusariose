2dad63fc398ac21a780ef6c6865b056c3df3240b
Filipe Andreis <filipe.andreis@LIVE.COM>
2021-10-17 17:38:26 -0300

    organização de camadas

---
+++ fusariose.cs
@@ -0,0 +1,11 @@
+﻿<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>net5.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <ProjectReference Include="..\Ftec.ProjetoWeb.Aplicacao1.Domain\Ftec.ProjetoWeb.Aplicacao1.Domain.csproj" />
+  </ItemGroup>
+
+</Project>
