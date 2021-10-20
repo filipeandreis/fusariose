939760a95c86f8fd3775ca4bc320678e48dd2b09
Filipe Andreis <filipe.andreis@LIVE.COM>
2021-10-20 18:58:56 -0300

    adapters

---
+++ fusariose.Application/Adapter/LoginAdapter.cs
@@ -0,0 +1,31 @@
+﻿using fusariose.Application.DTO;
+using fusariose.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fusariose.Application.Adapter
+{
+    public class LoginAdapter
+    {
+        public static LoginDTO ToLoginDTO(Login login)
+        {
+            return new LoginDTO()
+            {
+                Username = login.Username,
+                Password = login.Password
+            };
+        }
+
+        public static Login ToLoginDomain(LoginDTO login)
+        {
+            return new Login()
+            {
+                Username = login.Username,
+                Password = login.Password
+            };
+        }
+    }
+}
---
+++ fusariose.Application/Adapter/UserAdapter.cs
@@ -0,0 +1,31 @@
+﻿using fusariose.Application.DTO;
+using fusariose.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fusariose.Application.Adapter
+{
+    public class UserAdapter
+    {
+        public static UserDTO ToUserDTO(User user)
+        {
+            return new UserDTO()
+            {
+                Username = user.Username,
+                Password = user.Password
+            };
+        }
+
+        public static User ToUserDomain(UserDTO user)
+        {
+            return new User()
+            {
+                Username = user.Username,
+                Password = user.Password
+            };
+        }
+    }
+}
